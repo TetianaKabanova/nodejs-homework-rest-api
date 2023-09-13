2418a2329664ef0fe462af6417a3355ba4418ad9
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-09-13 13:46:13 +0200

    updated files

---
+++ routes/api/contacts-router.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+import contactsController from "../../controllers/contacts-controller.js";
+
+import contactsSchema from "../../schemas/contacts-schemas.js";
+
+import { validateBody } from "../../decorators/index.js";
+
+const contactAddValidate = validateBody(contactsSchema.contactAddSchema);
+
+const contactsRouter = express.Router();
+
+contactsRouter.get("/", contactsController.getAll);
+
+contactsRouter.get("/:id", contactsController.getById);
+
+contactsRouter.post("/", contactAddValidate, contactsController.add);
+
+contactsRouter.delete(
+  "/:id",
+  contactAddValidate,
+  contactsController.deleteById
+);
+
+contactsRouter.put("/:id", contactAddValidate, contactsController.updateById);
+
+export default contactsRouter;
