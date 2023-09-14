badfb31d2ebe61745208de64d5dda1b8d850b87e
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-09-14 18:23:50 +0200

    updated files

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+import mongoose from "mongoose";
+
+// const DB_HOST =
+//   "mongodb+srv://Tetiana:hyAwgdBbGf0iyjCo@cluster0.uql4gxm.mongodb.net/my-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
